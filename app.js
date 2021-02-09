311bbf9bf1319330dae0ee112c8c4df343831a84
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-02-09 01:02:42 -0400

    connect db mongo configuration

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const connectDB = require('./config/connectDB');
 const routes = require('./routes/index');
 const captureError = require('./utils/serverError')
 const app = express();
+
+connectDB(
+  process.env.URLDB,
+  process.env.PORTDB,
+  process.env.NAMEDB
+)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
---
+++ config/connectDB.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+module.exports = (url, name, port) => {
+  mongoose.Promise = global.Promise;
+  return new Promise((resolve, reject) => {
+    mongoose.connect(`mongodb://${url}:${port}/${name}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }).then(() => resolve(console.log('db is connect')))
+      .catch(err => reject(console.log(err)))
+  })
+}
