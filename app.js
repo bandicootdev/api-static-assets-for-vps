044d650df0145e313136f620614d5b2b41c1ac35
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-02-11 12:07:15 -0400

    controller getAll uploaded images

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ connectDB(
   process.env.PORTDB,
   process.env.NAMEDB
 )
-
+console.log(`${__dirname}/public`)
+app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public/images`))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
--- controllers/images.js
+++ controllers/images.js
@@ -2,7 +2,24 @@ const Image = require('../models/ImageModel')
 const {TYPES} = require("../utils/constants");
 
 module.exports.getListImages = async (req, res, next) => {
+  try {
+    const fullUrl = `${req.protocol}://${req.get('host')}`
+    let images = await Image.find({}).catch(err => {
+      throw err
+    })
+    images = images.reduce((acc, item) => {
+      let names = [];
+      item.images.forEach(item => {
+        names.push(`${fullUrl}/${item}`)
+      })
+      return acc.concat(names)
+    }, [])
 
+    return res.status(200).json({ok:true,images})
+  } catch (err) {
+    console.log(err)
+    next(err)
+  }
 }
 
 module.exports.uploadImagesController = async (req, res, next) => {
@@ -12,7 +29,7 @@ module.exports.uploadImagesController = async (req, res, next) => {
       if (!project || !type) {
         return res.status(400).json({ok: false, message: 'required fields'});
       }
-      if (typeof TYPES.find(el=>el.toLowerCase() === type) === "undefined") {
+      if (typeof TYPES.find(el => el.toLowerCase() === type) === "undefined") {
         return res.status(406).json({ok: false, message: 'invalid type'})
       }
       let images = req.files.reduce((acc, img) => {
